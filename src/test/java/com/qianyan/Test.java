c13dda015def330a1a7f7de19430156f6c37ffbb
鲁家宁 <facebooot@gmail.com>
2013-09-16 23:20:16 +0800

    test java

---
+++ src/test/java/com/qianyan/Test.java
@@ -0,0 +1,14 @@
+package com.qianyan;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-9-16
+ * Time: 下午11:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello qianyan");
+    }
+}
